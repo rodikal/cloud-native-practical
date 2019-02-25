f13157a47b2e6fba81d03911fb4948b20286c9ef
Samuel Van Reeth <samuel.vanreeth2@kbc.be>
2019-02-25 16:47:50 +0100

    {test} cocktail api test

---
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/web/cocktails/CocktailControllerTest.java
@@ -0,0 +1,54 @@
+package com.ezgroceries.shoppinglist.web.cocktails;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@ComponentScan("com.ezgroceries.shoppinglist.web")
+public class CocktailControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getAccountsTest() throws Exception {
+        this.mockMvc
+                .perform(get("/cocktails")
+                        .param("search", "Russian")
+                        .accept(MediaType.parseMediaType("application/json")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].name").value("Margerita"))
+                .andExpect(jsonPath("$[0].glass").value("Cocktail glass"))
+                .andExpect(jsonPath("$[0].image").value("https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg"))
+                .andExpect(jsonPath("$[0].ingredients.length()").value(4))
+                .andExpect(jsonPath("$[0].ingredients[0]").value("Tequila"))
+                .andExpect(jsonPath("$[0].ingredients[1]").value("Triple sec"))
+                .andExpect(jsonPath("$[0].ingredients[2]").value("Lime juice"))
+                .andExpect(jsonPath("$[0].ingredients[3]").value("Salt"))
+                .andExpect(jsonPath("$[1].name").value("Blue Margerita"))
+                .andExpect(jsonPath("$[1].glass").value("Cocktail glass"))
+                .andExpect(jsonPath("$[1].image").value("https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg"))
+                .andExpect(jsonPath("$[1].ingredients.length()").value(4))
+                .andExpect(jsonPath("$[1].ingredients[0]").value("Tequila"))
+                .andExpect(jsonPath("$[1].ingredients[1]").value("Blue Curacao"))
+                .andExpect(jsonPath("$[1].ingredients[2]").value("Lime juice"))
+                .andExpect(jsonPath("$[1].ingredients[3]").value("Salt"))
+        ;
+    }
+
+}
