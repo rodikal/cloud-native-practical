9f2a8ee8ca869e349d33e47ad418f4293a965b45
Samuel Van Reeth <samuel.vanreeth2@kbc.be>
2019-02-25 15:58:53 +0100

    {feature} add cocktail api

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
@@ -2,8 +2,10 @@ package com.ezgroceries.shoppinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ezgroceries.shoppinglist.web")
 public class ShoppingListApplication {
 
     public static void main(String[] args) {
