1fa613f4898ef0b0177328539795ba0af91d60be
Samuel Van Reeth <samuel.vanreeth2@kbc.be>
2019-02-28 12:59:53 +0100

    {feature} CocktailDB implementation

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/cocktails/web/CocktailController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/cocktails/web/CocktailController.java
@@ -1,9 +1,14 @@
 package com.ezgroceries.shoppinglist.cocktails.web;
 
+import com.ezgroceries.shoppinglist.cocktails.CocktailDBClient;
+import com.ezgroceries.shoppinglist.cocktails.CocktailDBResponse;
 import com.ezgroceries.shoppinglist.cocktails.CocktailResource;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,24 +19,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/cocktails", produces = "application/json")
 public class CocktailController {
 
-    @GetMapping
-    public ResponseEntity<List<CocktailResource>> get(@RequestParam String search) {
-        return ResponseEntity.ok(getDummyResources());
+
+    private CocktailDBClient cocktailDBClient;
+
+    @Autowired
+    public void setCocktailDBClient(CocktailDBClient cocktailDBClient) {
+        this.cocktailDBClient = cocktailDBClient;
     }
 
-    private List<CocktailResource> getDummyResources() {
-        return Arrays.asList(
-                new CocktailResource(
-                        UUID.fromString("23b3d85a-3928-41c0-a533-6538a71e17c4"), "Margerita",
-                        "Cocktail glass",
-                        "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten..",
-                        "https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg",
-                        Arrays.asList("Tequila", "Triple sec", "Lime juice", "Salt")),
-                new CocktailResource(
-                        UUID.fromString("d615ec78-fe93-467b-8d26-5d26d8eab073"), "Blue Margerita",
-                        "Cocktail glass",
-                        "Rub rim of cocktail glass with lime juice. Dip rim in coarse salt..",
-                        "https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg",
-                        Arrays.asList("Tequila", "Blue Curacao", "Lime juice", "Salt")));
+    @GetMapping
+    public ResponseEntity<List<CocktailResource>> get(@RequestParam String search) {
+
+        CocktailDBResponse cocktailDBResponse = cocktailDBClient.searchCocktails(search);
+        return ResponseEntity.ok(convert(cocktailDBResponse));
     }
+
+    private List<CocktailResource> convert(CocktailDBResponse dbResponse) {
+        return dbResponse.getDrinks().stream()
+                .map(drinkResource -> new CocktailResource(
+                                UUID.randomUUID(),
+                                drinkResource.getStrDrink(),
+                                drinkResource.getStrGlass(),
+                                drinkResource.getStrInstructions(),
+                                drinkResource.getStrDrinkThumb(),
+                                Stream.of(
+                                        drinkResource.getStrIngredient1(),
+                                        drinkResource.getStrIngredient2(),
+                                        drinkResource.getStrIngredient3(),
+                                        drinkResource.getStrIngredient4(),
+                                        drinkResource.getStrIngredient5()
+                                ).filter(StringUtils::isNotBlank).collect(Collectors.toList())
+                        )
+                ).collect(Collectors.toList());
+
+    }
+
 }
--- shopping-list/src/test/java/com/ezgroceries/shoppinglist/cocktails/web/CocktailControllerTest.java
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/cocktails/web/CocktailControllerTest.java
@@ -1,30 +1,39 @@
 package com.ezgroceries.shoppinglist.cocktails.web;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.ezgroceries.shoppinglist.cocktails.CocktailDBClient;
+import com.ezgroceries.shoppinglist.cocktails.CocktailDBResponse;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
-@ComponentScan("com.ezgroceries.shoppinglist.web")
+@WebMvcTest(CocktailController.class)
 public class CocktailControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    public CocktailDBClient cocktailDBClient;
+
     @Test
     public void getAccountsTest() throws Exception {
+
+        given(cocktailDBClient.searchCocktails("Russian")).willReturn(getCocktails());
         this.mockMvc
                 .perform(get("/cocktails")
                         .param("search", "Russian")
@@ -49,6 +58,40 @@ public class CocktailControllerTest {
                 .andExpect(jsonPath("$[1].ingredients[2]").value("Lime juice"))
                 .andExpect(jsonPath("$[1].ingredients[3]").value("Salt"))
         ;
+
+        verify(cocktailDBClient).searchCocktails("Russian");
+    }
+
+
+    private CocktailDBResponse getCocktails() {
+        List<CocktailDBResponse.DrinkResource> drinks = new ArrayList<>();
+        {
+            CocktailDBResponse.DrinkResource drinkResource = new CocktailDBResponse.DrinkResource();
+            drinkResource.setStrDrink("Margerita");
+            drinkResource.setStrGlass("Cocktail glass");
+            drinkResource.setStrInstructions("Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten..");
+            drinkResource.setStrDrinkThumb("https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg");
+            drinkResource.setStrIngredient1("Tequila");
+            drinkResource.setStrIngredient2("Triple sec");
+            drinkResource.setStrIngredient3("Lime juice");
+            drinkResource.setStrIngredient4("Salt");
+            drinks.add(drinkResource);
+        }
+        {
+            CocktailDBResponse.DrinkResource drinkResource = new CocktailDBResponse.DrinkResource();
+            drinkResource.setStrDrink("Blue Margerita");
+            drinkResource.setStrGlass("Cocktail glass");
+            drinkResource.setStrInstructions("Rub rim of cocktail glass with lime juice. Dip rim in coarse salt..");
+            drinkResource.setStrDrinkThumb("https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg");
+            drinkResource.setStrIngredient1("Tequila");
+            drinkResource.setStrIngredient2("Blue Curacao");
+            drinkResource.setStrIngredient3("Lime juice");
+            drinkResource.setStrIngredient4("Salt");
+            drinks.add(drinkResource);
+        }
+        CocktailDBResponse cocktailDBResponse = new CocktailDBResponse();
+        cocktailDBResponse.setDrinks(drinks);
+        return cocktailDBResponse;
     }
 
 }
